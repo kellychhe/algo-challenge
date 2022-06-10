cc56e516caa3bcb63c93d09c4f1e42edf09cbbf5
Kelly Chhe <kellykchhe@gmail.com>
2022-06-10 12:08:09 -0400

    algo-challenges

--- Beginner/factorial/index-START.js
+++ Beginner/factorial/index-START.js
@@ -4,7 +4,15 @@ the provided integer(n). E.g
     factorial(5) // should return 120
 */
 
+// function factorial(n){
+//     let product = 1
+//     for (let i = 1; i <= n; i++){
+//         product *= i
+//     }
+//     return product
+// }
 
+//
 function factorial(n, memo) {
     memo = memo || {}
     if(memo[n]){
@@ -15,5 +23,13 @@ function factorial(n, memo) {
     }
     return memo[n] = n * factorial(n - 1, memo)
 }
+
+// function factorial(n) {
+//     if (n === 0) {
+//         return 1
+//     }
+//     return n * factorial(n - 1)
+// }
+console.log(factorial(2))
 
 module.exports = factorial
